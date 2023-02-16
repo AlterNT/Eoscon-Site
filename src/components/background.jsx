455d107182e874b6c006faf6d28bfc5a1037c703
AlterNT <nathanphillis1@gmail.com>
2023-02-16 20:37:09 +0800

    Smooth scrolling + background stars

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,7 +1,7 @@
 import { onMount } from 'solid-js'
 import * as THREE from 'three'
-import { Line2 } from 'three/examples/jsm/lines/Line2'
-import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry'
+import { LineSegments2 } from 'three/examples/jsm/lines/LineSegments2'
+import { LineSegmentsGeometry } from 'three/examples/jsm/lines/LineSegmentsGeometry'
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial'
 import './background.scss'
 
@@ -9,6 +9,7 @@ export default function Background() {
   const canvas = <canvas></canvas>
 
   onMount(() => {
+    // Setup scene.
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(
       40,
@@ -16,29 +17,45 @@ export default function Background() {
       1,
       1000
     )
-    camera.position.set(0, 0, 10)
+    camera.position.set(0, 0, 200)
     camera.lookAt(0, 0, 0)
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
+      antialias: true,
     })
     renderer.setSize(window.innerWidth, window.innerHeight, false)
 
+    // Populate stars.
     const starmat = new LineMaterial({
       color: 0xffffff,
-      linewidth: 0.1,
+      linewidth: 0.4,
       worldUnits: true,
     })
 
-    const geo = new LineGeometry()
-    geo.setPositions([0, 0, 0, 0, 0.0001, 0])
+    // Parameters.
+    const STAR_COUNT = 10000
+    const STARS_WIDTH = 1000
+    const SPEED = 0.1
+    const STRETCH = 0.05
 
-    const line = new Line2(geo, starmat)
-    line.computeLineDistances()
-    line.scale.set(1, 1, 1)
+    // Randomly distribute stars.
+    let posarray = new Float32Array(6 * STAR_COUNT)
+    for (let i = 0; i < STAR_COUNT; i++) {
+      const x = 2 * STARS_WIDTH * Math.random() - STARS_WIDTH
+      const y = 2 * STARS_WIDTH * Math.random() - STARS_WIDTH
+      const z = -STARS_WIDTH * Math.random()
 
-    scene.add(line)
-    renderer.render(scene, camera)
+      posarray[6 * i] = x
+      posarray[6 * i + 1] = y
+      posarray[6 * i + 2] = z
+      posarray[6 * i + 3] = x
+      posarray[6 * i + 4] = y
+      posarray[6 * i + 5] = z + 0.01 // Negligible length so that the line renders.
+    }
+    const geometry = new LineSegmentsGeometry().setPositions(posarray)
+    const lines = new LineSegments2(geometry, starmat)
+    scene.add(lines)
 
     // Rescale updates.
     addEventListener('resize', () => {
@@ -47,6 +64,59 @@ export default function Background() {
       camera.updateProjectionMatrix()
       renderer.render(scene, camera)
     })
+
+    let vx = 0
+    let vy = 0
+    let vz = 0
+
+    // Capture scroll events.
+    addEventListener('smoothscroll', (e) => {
+      vy = e.detail.v
+      requestAnimationFrame(render)
+    })
+
+    const render = () => {
+      for (let i = 0; i < STAR_COUNT; i++) {
+        // Average position is center of line.
+        let x = (posarray[6 * i] + posarray[6 * i + 3]) / 2
+        let y = (posarray[6 * i + 1] + posarray[6 * i + 4]) / 2
+        let z = (posarray[6 * i + 2] + posarray[6 * i + 5]) / 2
+
+        // Loop stars around on edges of box. Slight offset to eliminate repeats.
+        if (x > STARS_WIDTH) {
+          x -= 2 * STARS_WIDTH
+          y += 20
+        } else if (x < -STARS_WIDTH) {
+          x += 2 * STARS_WIDTH
+          y -= 20
+        }
+        if (y > STARS_WIDTH) {
+          y -= 2 * STARS_WIDTH
+          z += 20
+        } else if (y < -STARS_WIDTH) {
+          y += 2 * STARS_WIDTH
+          z -= 20
+        }
+        if (z > 0) {
+          z -= STARS_WIDTH
+          x += 20
+        } else if (z < -STARS_WIDTH) {
+          z += STARS_WIDTH
+          x -= 20
+        }
+
+        posarray[6 * i] = x + vx * (SPEED + STRETCH)
+        posarray[6 * i + 1] = y + vy * (SPEED + STRETCH)
+        posarray[6 * i + 2] = z + vz * (SPEED + STRETCH)
+        posarray[6 * i + 3] = x + vx * (SPEED - STRETCH)
+        posarray[6 * i + 4] = y + vy * (SPEED - STRETCH)
+        posarray[6 * i + 5] = z + vz * (SPEED - STRETCH) + 0.01
+      }
+      geometry.setPositions(posarray)
+      renderer.render(scene, camera)
+    }
+
+    requestAnimationFrame(render)
   })
   return canvas
 }
--- src/root.jsx
+++ src/root.jsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { Suspense } from 'solid-js'
+import { onMount, Suspense } from 'solid-js'
 import {
   A,
   Body,
@@ -15,8 +15,70 @@ import {
 import Header from './components/header'
 import Background from './components/background'
 import './root.scss'
+import { lerp } from 'three/src/math/MathUtils'
 
 export default function Root() {
+  const wrapper = (
+    <div class='wrapper'>
+      <Header />
+      <Routes>
+        <FileRoutes />
+      </Routes>
+      <Scripts />
+    </div>
+  )
+  const body = (
+    <Body>
+      <Background />
+      {wrapper}
+    </Body>
+  )
+
+  const SMOOTHING = 0.2
+
+  let currentScroll = 0
+
+  // Smooth scrolling
+  onMount(() => {
+    body.style.height = wrapper.scrollHeight + 'px'
+    wrapper.style.position = 'fixed'
+
+    let moving = false
+
+    const render = () => {
+      const newScroll = lerp(currentScroll, window.scrollY, SMOOTHING)
+      // Fire custom event for other uses.
+      dispatchEvent(
+        new CustomEvent('smoothscroll', {
+          detail: {
+            y: newScroll,
+            v: newScroll - currentScroll,
+          },
+        })
+      )
+
+      currentScroll = newScroll
+      wrapper.style.translate = `0px -${currentScroll}px`
+      // Update until it's 'close enough'.
+      if (
+        currentScroll - window.scrollY > 0.1 ||
+        currentScroll - window.scrollY < -0.1
+      ) {
+        moving = true
+        requestAnimationFrame(render)
+      } else {
+        moving = false
+        wrapper.style.translate = `0px -${window.scrollY}px`
+      }
+    }
+
+    addEventListener('scroll', () => {
+      if (!moving) {
+        requestAnimationFrame(render)
+      }
+    })
+  })
+
   return (
     <Html lang='en'>
       <Head>
@@ -24,14 +86,7 @@ export default function Root() {
         <Meta charset='utf-8' />
         <Meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <Body>
-        <Background />
-        <Header />
-        <Routes>
-          <FileRoutes />
-        </Routes>
-        <Scripts />
-      </Body>
+      {body}
     </Html>
   )
 }
