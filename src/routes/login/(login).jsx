3b7b596161d9f4f913ac2a6945a6c13bbd0827e7
AlterNT <nathanphillis1@gmail.com>
2023-02-27 00:33:55 +0800

    started work on authentication.

--- src/routes/login/(login).jsx
+++ src/routes/login/(login).jsx
@@ -1,3 +1,22 @@
+import { onMount } from 'solid-js'
+
 export default function Login() {
-  return <main>Login</main>
+  const discord = <a class='button'>Sign in with Discord</a>
+  const steam = <a class='button'>Sign in with Steam</a>
+
+  onMount(() => {
+    discord.href = `https://discord.com/api/oauth2/authorize?client_id=1078192080364507246&redirect_uri=${encodeURIComponent(
+      window.location.origin
+    )}&response_type=code&scope=identify`
+  })
+
+  return (
+    <main>
+      <div class='panel'>
+        <h1>Login</h1>
+        {discord}
+        {steam}
+      </div>
+    </main>
+  )
 }
